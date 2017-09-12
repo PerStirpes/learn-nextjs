15e05ec6b9f0b91e1d789d1e3bb652fbe7f98de3
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-09-11 20:16:20 -0700

    Complete Nav Btwn Pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <button style={{ fontSize: 20 }}>About Page</button>
+    </Link>
     <p>Hello Next.js</p>
+    Click{' '}
+    <Link href="/about">
+      <a>here</a>
+    </Link>{' '}
+    to read more
   </div>
-)
+);
 
-export default Index
+export default Index;
+//
+// import Link from 'next/link';
+//
+// export default () => (
+//   <div>
+//     Click{' '}
+//     <Link href="/about">
+//       <a>here</a>
+//     </Link>{' '}
+//     to read more
+//   </div>
+// );
