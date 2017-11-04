2661670610bbf6952bb1105cb398a245fb1e5c7d
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-11-04 03:38:36 -0700

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -1,5 +1,7 @@
 import Layout from '../components/MyLayout.js'
 
+//Our /post page accepts the title via the query string parameter title. In client side routing, we can easily give it a proper value with URL masking.
+//(via the as prop in Link).
 export default (props) => (
     <Layout>
        <h1>{props.url.query.title}</h1>
