c1f99d44c38f4e8760db3a1b0f386e1a46095d5a
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-11-04 02:55:00 -0700

    complete Clean URLs with Route Masking

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
-// This is Link API
-
 import Layout from '../components/MyLayout.js';
 import Link from 'next/link'
 
 const PostLink = (props) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-    <a>{props.title}</a>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
     </Link>
   </li>
 )
+
 export default () =>  (
   <Layout>
     <h1>Hello Next.js</h1>
-    <h2>My Blog</h2>
     <ul>
-      <PostLink title="stuff i've learned"/>
-      <PostLink title="random"/>
-      <PostLink title="Learn Next.js is awesome"/>
-      <PostLink title="Deploy apps with Zeit"/>
+      <PostLink id="stuff-learned" title="stuff ive learned"/>
+      <PostLink id="random" title="random"/>
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+      <PostLink id="deploy" title="Deploy apps with Zeit"/>
     </ul>
   </Layout>
 );
